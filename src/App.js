8c2957bf27db5183ac6afc878d6cb5971e638e55
luzybry94 <luzybry94@gmail.com>
2021-05-26 02:20:17 -0700

    removed boiler plate and added routing/navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Switch, Route } from "react-router-dom";
+// import Home from "./components/Home";
+import TripList from "./components/Trip/TripList";
+import TripForm from "./components/Trip/TripForm";
+// import TripShow from "./components/Trip/TripShow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/trips" component={TripList} />
+          <Route exact path="/trips/new" component={TripForm} />
+          {/* <Route path="/trip/:id" component={TripShow} /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
