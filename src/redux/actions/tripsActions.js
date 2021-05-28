f99c2388941aa39c41cd4481edc7be00e10ff320
luzybry94 <luzybry94@gmail.com>
2021-05-28 12:55:51 -0700

    adds bearer token to requests, updates url and redirects user after dispatch

--- src/redux/actions/tripsActions.js
+++ src/redux/actions/tripsActions.js
@@ -1,24 +1,30 @@
-export const getTrips = () => {
+export const getTrips = (userId) => {
   return (dispatch) => {
-    fetch("http://localhost:3000/api/v1/trips")
+    fetch(`http://localhost:3000/api/v1/users/${userId}/trips`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
       .then((res) => res.json())
-      .then((data) => dispatch({ type: "GET_TRIPS", payload: data }));
+      .then((data) => dispatch({ type: "SET_TRIPS", payload: data }));
   };
 };
 
-export const createTrip = (newTripData) => {
+export const createTrip = (newTripData, userId, history) => {
   return (dispatch) => {
-    fetch("http://localhost:3000/api/v1/trips", {
+    fetch(`http://localhost:3000/api/v1/users/${userId}/trips`, {
       method: "POST",
       headers: {
         Accepts: "application/json",
         "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
       },
-      body: JSON.stringify({ trip: newTripData }),
+      body: JSON.stringify(newTripData),
     })
       .then((res) => res.json())
       .then((data) => {
         dispatch({ type: "ADD_TRIP", payload: data });
+        history.push("/trips");
       });
   };
 };
