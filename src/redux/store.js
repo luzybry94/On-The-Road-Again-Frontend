a61c6e532d7397e02cc351ba29f67cbc259fb6ec
luzybry94 <luzybry94@gmail.com>
2021-05-28 12:54:33 -0700

    adds current user reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import { trips } from "./reducers/tripsReducer";
 import { usaStates } from "./reducers/usaStatesReducer";
 import thunk from "redux-thunk";
+import { currentUser } from "./reducers/usersReducer";
 
 const rootReducer = combineReducers({
   trips,
   usaStates,
+  currentUser,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
