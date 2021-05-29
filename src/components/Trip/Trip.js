a4f2c960b5b161196ab1f105ff358b8f12c5a6c5
luzybry94 <luzybry94@gmail.com>
2021-05-28 17:02:53 -0700

    converts to functional comp and adds Link

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-class Trip extends Component {
-  render() {
-    const { name, dateStart, dateEnd, img, states } = this.props.trip;
-    return (
-      <div className="trip-card">
-        <img src={img} alt="trip-image" />
-        <h3>{name}</h3>
-        <h4>States: {states.map((state) => `${state.name} `)}</h4>
-        <h6>
-          {dateStart} - {dateEnd}
-        </h6>
-      </div>
-    );
-  }
-}
+const Trip = ({ name, dateStart, dateEnd, img, states, id }) => {
+  return (
+    <div className="trip-card">
+      <img style={{ display: "block" }} src={img} alt="trip-image" />
+      <h3 style={{ display: "inline" }}>{name} </h3>
+      <span>
+        <Link to={`/trips/${id}`}>&#40;See Attractions&#41;</Link>
+      </span>
+      <h4>States: {states.map((state) => `${state.name} `)}</h4>
+      <h6>
+        {dateStart} - {dateEnd}
+      </h6>
+    </div>
+  );
+};
 
 export default Trip;
