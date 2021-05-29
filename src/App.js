a9d9c3dfb9bcd51e87802cbd7cd1d4da15b98185
luzybry94 <luzybry94@gmail.com>
2021-05-28 17:01:41 -0700

    removes logic from app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
-import TripList from "./components/Trip/TripList";
-import TripForm from "./components/Trip/TripForm";
-// import TripShow from "./components/Trip/TripShow";
+import TripContainer from "./containers/TripContainer";
 import { connect } from "react-redux";
 import { autoLogin } from "./redux/actions/userActions";
-import Login from "./components/Login";
 
 class App extends React.Component {
   componentDidMount() {
@@ -18,37 +15,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {this.props.user.id ? (
-          <>
-            <NavBar />
-            <Switch>
-              <Route
-                exact
-                path="/trips"
-                render={() => (
-                  <TripList trips={this.props.trips} user={this.props.user} />
-                )}
-              />
-              <Route exact path="/trips/new" component={TripForm} />
-              {/* <Route path="/trip/:id" component={TripShow} /> */}
-            </Switch>
-          </>
-        ) : (
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        )}
+        <NavBar />
+        <Switch>
+          <Route path="/trips" component={TripContainer} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.currentUser,
-    trips: state.trips,
-  };
-};
-
-export default connect(mapStateToProps, { autoLogin })(App);
+export default connect(null, { autoLogin })(App);
