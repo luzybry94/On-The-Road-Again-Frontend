96c1df4858cbe79cdde8a5bdc51b619e7d94a1c3
luzybry94 <luzybry94@gmail.com>
2021-05-28 17:03:15 -0700

    converts to functional comp and removes logic

--- src/components/Trip/TripList.js
+++ src/components/Trip/TripList.js
@@ -1,24 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Trip from "./Trip";
-import { Redirect } from "react-router-dom";
 
-class TripList extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div className="trip-container">
-        {this.props.user.id ? (
-          this.props.trips.length > 0 ? (
-            this.props.trips.map((trip) => <Trip key={trip.id} trip={trip} />)
-          ) : (
-            <h3>Loading...</h3>
-          )
-        ) : (
-          <Redirect to="/" />
-        )}
-      </div>
-    );
-  }
-}
+const TripList = ({ trips }) => {
+  return (
+    <div>
+      {trips.map((trip) => (
+        <Trip key={trip.id} {...trip} />
+      ))}
+    </div>
+  );
+};
 
 export default TripList;
