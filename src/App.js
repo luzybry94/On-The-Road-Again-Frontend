4834b0707b129be9cef783b6cf50ffd3f3280d0e
luzybry94 <luzybry94@gmail.com>
2021-05-28 19:04:53 -0700

    adds loading state and toggle loading functionality for CDM

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import "./App.css";
 import React from "react";
 import NavBar from "./components/NavBar";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import TripContainer from "./containers/TripContainer";
 import { connect } from "react-redux";
-import { autoLogin } from "./redux/actions/userActions";
+import { autoLogin } from "./redux/actions/authActions";
 
 class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  toggleLoading = () => {
+    this.setState({ loading: !this.state.loading });
+  };
+
   componentDidMount() {
-    localStorage.token && this.props.autoLogin();
+    if (localStorage.token) {
+      this.props.autoLogin(this.toggleLoading);
+    } else {
+      this.toggleLoading();
+    }
   }
 
   render() {
+    if (this.state.loading) return <h1>Loading...</h1>;
     return (
       <div className="App">
         <NavBar />
         <Switch>
-          <Route path="/trips" component={TripContainer} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/trips"
+            render={(props) => {
+              if (this.props.loggedIn) {
+                return <TripContainer {...props} />;
+              } else {
+                return <Redirect to="/" />;
+              }
+            }}
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default connect(null, { autoLogin })(App);
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.loggedIn,
+  };
+};
+
+export default connect(mapStateToProps, { autoLogin })(App);
