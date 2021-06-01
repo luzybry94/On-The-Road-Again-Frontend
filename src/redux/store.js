8b99a9f8b0b3ba1190693551c6c74d514d896bfb
luzybry94 <luzybry94@gmail.com>
2021-06-01 03:13:32 -0700

    adds attractions reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { trips } from "./reducers/tripsReducer";
 import { usaStates } from "./reducers/usaStatesReducer";
-import thunk from "redux-thunk";
 import { auth } from "./reducers/authReducer";
+import { attractions } from "./reducers/attractionsReducer";
 
 const rootReducer = combineReducers({
   trips,
   usaStates,
   auth,
+  attractions,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
