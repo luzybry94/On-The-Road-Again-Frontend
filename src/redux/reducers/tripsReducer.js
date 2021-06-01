3b3a048951f54332c69080fd78660843c4ebafb0
luzybry94 <luzybry94@gmail.com>
2021-06-01 15:18:12 -0700

    adds deleting and adding todo cases

--- src/redux/reducers/tripsReducer.js
+++ src/redux/reducers/tripsReducer.js
@@ -4,7 +4,15 @@ export const trips = (state = [], action) => {
       return action.payload;
     case "ADD_TRIP":
       return [...state, action.payload];
+    case "ADD_TODO":
+      return state.map((trip) =>
+        trip.id === action.payload.id ? action.payload : trip
+      );
     case "UPDATE_TODO":
+      return state.map((trip) =>
+        trip.id === action.payload.id ? action.payload : trip
+      );
+    case "DELETE_TODO":
       return state.map((trip) =>
         trip.id === action.payload.id ? action.payload : trip
       );
