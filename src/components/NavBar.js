2317f9fd7ddc3d75823fe89f4520294617d47725
luzybry94 <luzybry94@gmail.com>
2021-06-01 02:57:01 -0700

    connects to redux and adds navlink rendering logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../redux/actions/authActions";
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="nav-bar">
-        <NavLink className="nav-link" to="/trips">
-          My Trips
-        </NavLink>
-        <NavLink className="nav-link" to="/trips/new">
-          Plan a New Trip
-        </NavLink>
-        <NavLink className="nav-link" to="/logout">
-          Logout
-        </NavLink>
+        {this.props.loggedIn ? (
+          <>
+            <NavLink className="nav-link" to="/trips">
+              My Trips
+            </NavLink>
+            <NavLink className="nav-link" to="/trips/new">
+              Plan a New Trip
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="#"
+              onClick={() => this.props.logout()}
+            >
+              Logout
+            </NavLink>
+          </>
+        ) : null}
       </div>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.loggedIn,
+  };
+};
+
+export default connect(mapStateToProps, { logout })(Navbar);
