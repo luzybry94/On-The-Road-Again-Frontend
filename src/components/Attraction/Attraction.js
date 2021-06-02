4a43e37221a453e0673e4a8d36f3648df19b2203
luzybry94 <luzybry94@gmail.com>
2021-06-02 15:31:07 -0700

    updates date using moment

--- src/components/Attraction/Attraction.js
+++ src/components/Attraction/Attraction.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Moment from "react-moment";
 
-const Attraction = ({ name, pricing, dateLg, img, location }) => {
+const Attraction = ({ name, pricing, date, img, location }) => {
   return (
     <div className="attraction-card">
       <img src={img} alt="attraction" />
       <h1>{name}</h1>
       <h3>Where: {location}</h3>
       <h4>Pricing: {pricing}</h4>
-      <h6>When: {dateLg}</h6>
+      <h6>
+        When: <Moment date={date} format="LL" />
+      </h6>
     </div>
   );
 };
