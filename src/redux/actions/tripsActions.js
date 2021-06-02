3543e0215cdc7f08e8156beedc4de147be0ec551
luzybry94 <luzybry94@gmail.com>
2021-06-02 15:40:25 -0700

    adds update, getTripFormData and delete actions

--- src/redux/actions/tripsActions.js
+++ src/redux/actions/tripsActions.js
@@ -24,8 +24,53 @@ export const createTrip = (newTripData, history) => {
       .then((res) => res.json())
       .then((data) => {
         dispatch({ type: "ADD_TRIP", payload: data });
-        console.log(data);
         history.push("/trips");
       });
+  };
+};
+
+export const updateTrip = (updatedTripData, tripId, history) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/trips/${tripId}`, {
+      method: "PATCH",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+      body: JSON.stringify(updatedTripData),
+    })
+      .then((res) => res.json())
+      .then((trip) => {
+        dispatch({ type: "UPDATE_TRIP", payload: trip });
+        history.push("/trips");
+      });
+  };
+};
+
+export const getTripData = (tripId, callback) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/trips/${tripId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((trip) => {
+        dispatch({ type: "SET_TRIP_DATA", payload: trip });
+        callback(trip);
+      });
+  };
+};
+
+export const deleteTrip = (tripId) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/trips/${tripId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "SET_TRIPS", payload: data }));
   };
 };
