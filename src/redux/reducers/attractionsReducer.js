3bfb32459abf51e92230f205dc8b41421ddf777c
luzybry94 <luzybry94@gmail.com>
2021-06-03 20:54:05 -0700

    adds update and delete cases

--- src/redux/reducers/attractionsReducer.js
+++ src/redux/reducers/attractionsReducer.js
@@ -4,6 +4,12 @@ export const attractions = (state = [], action) => {
       return action.payload;
     case "ADD_ATTRACTION":
       return [...state, action.payload];
+    case "UPDATE_ATTRACTION":
+      return state.map((attraction) =>
+        attraction.id === action.payload.id ? action.payload : attraction
+      );
+    case "DELETE_ATTRACTION":
+      return state.filter((attraction) => attraction.id !== action.payload);
     case "UNSET_ATTRACTIONS":
       return [];
 
