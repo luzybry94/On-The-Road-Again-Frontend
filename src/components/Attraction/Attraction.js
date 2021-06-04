47b17cdfead3da8be3e353327ff568831fbf3485
luzybry94 <luzybry94@gmail.com>
2021-06-03 20:42:52 -0700

    updates to class component to allow for edit mode

--- src/components/Attraction/Attraction.js
+++ src/components/Attraction/Attraction.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import Moment from "react-moment";
+import AttractionEditForm from "./AttractionEditForm";
 
-const Attraction = ({ name, pricing, date, img, location }) => {
-  return (
-    <div className="attraction-card">
-      <img src={img} alt="attraction" />
-      <h1>{name}</h1>
-      <h3>Where: {location}</h3>
-      <h4>Pricing: {pricing}</h4>
-      <h6>
-        When: <Moment date={date} format="LL" />
-      </h6>
-    </div>
-  );
-};
+class Attraction extends Component {
+  state = {
+    editMode: false,
+  };
+
+  toggleEditMode = () => {
+    this.setState({
+      editMode: !this.state.editMode,
+    });
+  };
+
+  renderPricing = (pricing) => {
+    switch (pricing) {
+      case "Free":
+        return "Free";
+      case "Cheap":
+        return "$";
+      case "Moderate":
+        return "$$";
+      case "Expensive":
+        return "$$$";
+      default:
+        return "Unknown";
+    }
+  };
+
+  render() {
+    if (this.state.editMode)
+      return (
+        <AttractionEditForm
+          attraction={this.props}
+          toggleEditMode={this.toggleEditMode}
+        />
+      );
+    const { name, img, location, pricing, date } = this.props;
+    return (
+      <div className="attraction-card">
+        <img src={img} alt="attraction" />
+        <h1>{name}</h1>
+
+        <h3>{location}</h3>
+        <h4>Pricing: {this.renderPricing(pricing)}</h4>
+        <h5>
+          <Moment date={date} format="LL" />
+        </h5>
+        <button onClick={this.toggleEditMode}>Edit</button>
+      </div>
+    );
+  }
+}
 
 export default Attraction;
