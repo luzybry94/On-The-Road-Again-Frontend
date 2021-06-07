644b709aca356baacdafeb1a41a67b4eab9f8388
luzybry94 <luzybry94@gmail.com>
2021-06-07 13:22:42 -0700

    adds loading and toggling attraction form functionality

--- src/components/Trip/TripShow.js
+++ src/components/Trip/TripShow.js
@@ -5,24 +5,58 @@ import {
   unsetAttractions,
 } from "../../redux/actions/attractionsActions";
 import AttractionList from "../Attraction/AttractionList";
+import AttractionForm from "../Attraction/AttractionForm";
 
 class TripShow extends Component {
+  state = {
+    loading: true,
+    showAttractionForm: false,
+  };
   componentDidMount() {
-    this.props.getAttractions(this.props.tripId);
+    this.props.getAttractions(this.props.tripId, this.toggleLoading);
   }
+
+  toggleLoading = () => {
+    this.setState({
+      loading: !this.state.loading,
+    });
+  };
+
+  toggleShowAttrForm = () => {
+    this.setState({
+      showAttractionForm: !this.state.showAttractionForm,
+    });
+  };
 
   componentWillUnmount() {
     this.props.unsetAttractions();
   }
 
   render() {
+    if (this.state.loading)
+      return <h1 style={{ textAlign: "center" }}>Loading...</h1>;
     const { attractions, tripId } = this.props;
     return (
       <div>
         {attractions.length > 0 ? (
           <AttractionList attractions={attractions} tripId={tripId} />
         ) : (
-          <h1 style={{ textAlign: "center" }}>Loading...</h1>
+          <>
+            <h1 style={{ textAlign: "center" }}>No Attractions Planned</h1>
+            {this.state.showAttractionForm ? (
+              <AttractionForm
+                toggleShowAttrForm={this.toggleShowAttrForm}
+                tripId={this.props.tripId}
+              />
+            ) : (
+              <button
+                className="add-attraction-card"
+                onClick={this.toggleShowAttrForm}
+              >
+                Add Attraction
+              </button>
+            )}
+          </>
         )}
       </div>
     );
