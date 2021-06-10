f6b0236a780df8520de4734dc82b10e91c963890
luzybry94 <luzybry94@gmail.com>
2021-06-09 17:50:01 -0700

    removes inline styling

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -31,41 +31,39 @@ class Trip extends Component {
     const { name, startDate, endDate, img, states, id, todos } = this.props;
     return (
       <div className="trip-card">
-        <img
-          style={{ display: "block", marginBottom: "25px" }}
-          src={img}
-          alt="trip"
-        />
-        <h3 style={{ display: "inline" }}>{name}</h3>
-        <span style={{ marginLeft: "10px" }}>
-          <Link to={`/trips/${id}`}>&#40;See Attractions&#41;</Link>
-        </span>
-        <button onClick={this.toggleEditMode} style={{ float: "right" }}>
-          Edit Trip
-        </button>
-        <p>
-          {<Moment date={startDate} format="LL" />} -
-          {<Moment date={endDate} format="LL" />}
-        </p>
-
-        <p>
-          {states.map((state) => (
-            <li key={state.id}>{state.name}</li>
-          ))}
-        </p>
-
-        {this.state.showTodos ? (
-          <>
-            <button type="button" onClick={this.toggleTodos}>
-              Close
-            </button>
-            <TodoContainer tripId={id} todos={todos} />
-          </>
-        ) : (
-          <button type="button" onClick={this.toggleTodos}>
-            ToDos
+        <img src={img} alt="trip" />
+        <div className="trip-info">
+          <h3 style={{ display: "inline" }}>{name}</h3>
+          <span style={{ marginLeft: "10px" }}>
+            <Link to={`/trips/${id}`}>&#40;See Attractions&#41;</Link>
+          </span>
+          <button onClick={this.toggleEditMode} style={{ float: "right" }}>
+            Edit Trip
           </button>
-        )}
+          <p>
+            {<Moment date={startDate} format="LL" />} -
+            {<Moment date={endDate} format="LL" />}
+          </p>
+
+          <p>
+            {states.map((state) => (
+              <li key={state.id}>{state.name}</li>
+            ))}
+          </p>
+
+          {this.state.showTodos ? (
+            <>
+              <button type="button" onClick={this.toggleTodos}>
+                Close
+              </button>
+              <TodoContainer tripId={id} todos={todos} />
+            </>
+          ) : (
+            <button type="button" onClick={this.toggleTodos}>
+              ToDos
+            </button>
+          )}
+        </div>
       </div>
     );
   }
